a97654027c7e3365ec1a000c44df64f30d6c2d8a
Kanishk Chinna <kanishkchinna@kanishks-mbp.lan>
2021-08-16 18:19:52 -0700

    updated colors and fixed mobile responsiveness

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ window.onload = function() {
 function parallax_height() {
     let scroll_top = $(this).scrollTop();
     let sample_section_top = $('.main-content').offset().top;
-    let header_height = $('.header-section').outerHeight() - scroll_top - 20;
+    let header_height = $('.header-section').outerHeight() - scroll_top + 20;
     $('.main-content').css({ 'margin-top': header_height});
     // $('.title-header').css({ height: header_height - scroll_top + 24});
     $('.title-header').css({ height: window.innerHeight});
@@ -35,23 +35,12 @@ const toTop = document.getElementById('top');
 window.addEventListener('scroll', function(event) {
     if (window.pageYOffset > 100) {
         toTop.classList.add('active');
+        console.log("hello")
     }
     else {
         toTop.classList.remove('active');
     }
 })
-
-
-// const nameTop = document.getElementById('titlename');
-// // nameTop.classList.add('active');
-// window.addEventListener('scroll', function(event) {
-//     if (window.pageYOffset > 200) {
-//         nameTop.style.visibility = "hidden";
-//     }
-//     else {
-//         nameTop.style.visibility = "visible";
-//     }
-// })
 
 
 window.onbeforeunload = () => {
