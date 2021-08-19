714eb8c86231cb40d627b92c5cc57227180f8947
Kanishk Chinna <kanishkchinna@kanishks-mbp.lan>
2021-08-18 23:09:23 -0700

    hamburger menu transforms

--- script.js
+++ script.js
@@ -4,8 +4,65 @@ $(window).on('beforeunload', function(){
       $(window).scrollTop();
 });
 
+function parallax_height() {
+    let scroll_top = $(this).scrollTop();
+    let sample_section_top = $('.main-content').offset().top;
+    // let header_height = $('.header-section').outerHeight() - scroll_top;
+    $('.main-content').css({ 'margin-top': window.innerHeight});
+    // $('.title-header').css({ height: header_height - scroll_top + 24});
+    $('.title-header').css({ height: window.innerHeight});
+}
+parallax_height()
+
+
+//Back to Top Button
+const toTop = document.getElementById('top');
+window.addEventListener('scroll', function(event) {
+    if (window.pageYOffset > 500) {
+        toTop.classList.add('active');
+    }
+    else {
+        toTop.classList.remove('active');
+    }
+})
+
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
+
+$(document).ready(function(){
+    let isChrome = navigator.userAgent.indexOf('Chrome')
+    if (isChrome <= -1) {
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+    
+            // Store hash
+            var hash = this.hash;
+    
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+        
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+            });
+        } // End if
+        });
+    }
+});
+
 window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    document.getElementById('contact').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
@@ -18,35 +75,5 @@ window.onload = function() {
             });
     });
 }
-
-function parallax_height() {
-    let scroll_top = $(this).scrollTop();
-    let sample_section_top = $('.main-content').offset().top;
-    // let header_height = $('.header-section').outerHeight() - scroll_top;
-    $('.main-content').css({ 'margin-top': window.innerHeight - 20});
-    // $('.title-header').css({ height: header_height - scroll_top + 24});
-    $('.title-header').css({ height: window.innerHeight});
-}
-parallax_height()
-
-
-//Back to Top Button
-const toTop = document.getElementById('top');
-window.addEventListener('scroll', function(event) {
-    if (window.pageYOffset > 100) {
-        toTop.classList.add('active');
-        console.log("hello")
-    }
-    else {
-        toTop.classList.remove('active');
-    }
-})
-
-
-window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
-    }
-  }
 
 
